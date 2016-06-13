64a2ac1be2fbb5a64ac51e95bd4ba607bbe36adc
TK <ngtiankiat97@gmail.com>
2016-06-13 12:54:39 +0800

    Added MT v0.7
    + ShotGun
    + SmarterA Sharphead AI
    + Laser Pistol
    + Updated Weapons system

---
+++ MT_v0.7/Assets/Scripts/Game/BasicBullet.cs
@@ -0,0 +1,22 @@
+﻿// ===============================
+// AUTHOR: Ng Tian Kiat
+// CREATE DATE:
+// Description: BasicBullet is 
+// ===============================
+/* Change History:
+
+*/
+using UnityEngine;
+using System.Collections;
+
+public class BasicBullet : BaseProjectile {
+
+	void OnTriggerEnter() {
+		HitTarget();
+	}
+
+	public override void HitTarget() {
+		// Debug.Log("I Hit!");
+		Destroy(gameObject);
+	}
+}
---
+++ MT_v0.7/Assets/Scripts/Game/DrivingSystem.cs
@@ -0,0 +1,13 @@
+﻿// ===============================
+// AUTHOR: Ng Tian Kiat
+// CREATE DATE:
+// Description: DrivingSystem is the script to handle input from the players into the vehicle
+// ===============================
+/* Change History:
+
+*/
+using UnityEngine;
+using System.Collections;
+
+public class DrivingSystem : BaseInteractable {
+}
